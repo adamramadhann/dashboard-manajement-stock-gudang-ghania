e793d1bf41721f540395730b032b2e33a0cfa02a
Adam Ramadhan <ramadhanadam1104@gmail.com>
2026-01-26 20:15:53 +0700

    add: page stock with table and buton crud

--- src/page/Stock.jsx
+++ src/page/Stock.jsx
@@ -1,8 +1,57 @@
+import { Button, Table } from 'antd';
 import React from 'react'
 
 const Stock = () => {
+    const columns = [
+        {
+          title: 'No',
+          dataIndex: '',
+          key: 'no',
+        },
+        {
+          title: 'Name Product',
+          dataIndex: 'name_product',
+          key: 'nameStock',
+        },
+        {
+          title: 'Stock Stock',
+          dataIndex: 'stock_product',
+          key: 'Stock',
+        },
+        {
+          title: 'Status Product',
+          dataIndex: 'status_product',
+          key: 'StatusProduct',
+        },
+        {
+          title: 'Action',
+          dataIndex: '',
+          key: 'Action',
+        },
+      ];
+
+    const data = [
+        {
+            name_product: "Momogi",
+            stock_product: 10,
+            status_product: "Out"
+        }
+    ];
   return (
-    <div>Stock</div>
+    <div>
+         <div className='flex items-center justify-between mb-5' >
+            <h1 className='text-lg text-gray-700 font-semibold' >
+            Stock History Product
+            </h1>
+            <Button variant='outlined' color='primary' >
+            Create Stock Product
+            </Button>
+        </div>
+        <Table 
+            dataSource={data}
+            columns={columns} 
+        />
+    </div>
   )
 }
 
